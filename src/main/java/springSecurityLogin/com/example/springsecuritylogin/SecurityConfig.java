18ec86004b512f11111d46cb54354c3c85f302e6
Raed Majeed <raedam786@gmail.com>
2023-08-09 16:25:56 +0530

    initial commit

---
+++ src/main/java/springSecurityLogin/com/example/springsecuritylogin/SecurityConfig.java
@@ -0,0 +1,61 @@
+package springSecurityLogin.com.example.springsecuritylogin;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain (HttpSecurity http) throws  Exception {
+        http.
+                headers(header ->
+                        header.cacheControl(HeadersConfigurer.CacheControlConfig::disable)
+                );
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth ->
+                        auth
+                                .requestMatchers("/", "/login").permitAll()
+                                .requestMatchers("/welcome").authenticated()
+                )
+                .anonymous(AbstractHttpConfigurer::disable)
+                .formLogin(form ->
+                        form
+                                .loginPage("/login")
+                                .loginProcessingUrl("/login")
+                                .defaultSuccessUrl("/welcome", true) // why even after setting true redirects to login page?
+                                .failureUrl("/login?error")
+                                .permitAll()
+                )
+                .logout(logout -> logout.logoutSuccessUrl("/login")
+                .deleteCookies("JSESSIONID")
+                );
+        return http.build();
+
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager(PasswordEncoder encoder) {
+        UserDetails user = User.withUsername("user")
+                .password(encoder.encode("password"))
+                .build();
+        return  new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ src/test/java/springSecurityLogin/com/example/springsecuritylogin/SpringsecurityloginApplicationTests.java
@@ -0,0 +1,13 @@
+package springSecurityLogin.com.example.springsecuritylogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityloginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
