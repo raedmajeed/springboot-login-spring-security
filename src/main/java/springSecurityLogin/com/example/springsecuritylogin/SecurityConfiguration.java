65582ed859e1b19a40e41f0ca406f2e6c9e06abe
Raed Majeed <raedam786@gmail.com>
2023-08-09 19:44:51 +0530

    final commit

--- src/main/java/springSecurityLogin/com/example/springsecuritylogin/SecurityConfiguration.java
+++ src/main/java/springSecurityLogin/com/example/springsecuritylogin/SecurityConfiguration.java
@@ -11,7 +11,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;]
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
